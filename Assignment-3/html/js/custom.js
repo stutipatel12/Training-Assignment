f7a951f4c445fa7e6c1c203dbbaa5ee5e10dde53
stuti patel <stutipatel321@gmail.com>
2022-04-15 11:29:25 +0530

    commit

--- Assignment-3/html/js/custom.js
+++ Assignment-3/html/js/custom.js
@@ -142,8 +142,8 @@ $(document).ready(function() {
       {
         breakpoint: 1440,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 4,
+          slidesToScroll: 4,
           infinite: true,
           dots: true
         }
@@ -151,8 +151,8 @@ $(document).ready(function() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 4,
+          slidesToScroll: 4,
           infinite: true,
           dots: true
         }
