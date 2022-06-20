7ea23ee79fd3ad8480435a342d1a48dd5280d774
stuti patel <stutipatel321@gmail.com>
2022-06-20 10:21:43 +0530

    commit

--- Assignment-6/assets/js/general.js
+++ Assignment-6/assets/js/general.js
@@ -191,3 +191,38 @@ AOS.init({
   easing: 'ease', 
 }
 );
+
+function header_adj() {
+	var header_height = $(".header").outerHeight();
+	$(".main-wrap").css({ "padding-top": header_height + "px" });  
+  $(".header").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".img-wrapper").offset().top -  header_height
+    }, 1500);
+});
+}
+
+$(document).ready(function(){
+    header_adj();
+  });
+$(window).on("resize", function(){
+  header_adj();
+})
+
+
+function footer_adj() {
+  var footer_height = $("footer").outerHeight();
+  $(".wrapper").css({
+      "padding-bottom": footer_height + "px"
+  });
+  $("footer").css({
+      "margin-top": -footer_height + "px"
+  });
+}
+
+$(document).ready(function(){
+  footer_adj();
+});
+$(window).on("resize", function(){
+  footer_adj();
+})
