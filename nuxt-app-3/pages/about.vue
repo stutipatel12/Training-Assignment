0daa45acdae475f06932f27e2c4cdccc7a10a380
stuti patel <stutipatel321@gmail.com>
2022-09-02 10:14:12 +0530

    commit

---
+++ nuxt-app-3/pages/about.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>about page</h1>
+</template>
+
---
+++ nuxt-app-3/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Name Generator</h1>
+    <p>Choose your options and click the "find name" button below</p>
+    <div class="options-container">
+        <div class="option-container">
+            <h4>1) Choose a gender</h4>
+            <div class="option-buttons">
+                <button>Boy</button>
+                <button>Girl</button>
+            </div>
+        </div>
+        <div class="option-container">
+            <h4>2) Choose the name's popularity</h4>
+            <div class="option-buttons">
+                <button>Trendy</button>
+                <button>Unique</button>
+            </div>
+        </div>
+        <div class="option-container">
+            <h4>3) Choose name's length</h4>
+            <div class="option-buttons">
+                <button>Short</button>
+                <button>Long</button>
+                <button>All</button>
+            </div>
+        </div>
+    </div>
+</template>
